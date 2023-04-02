a9276cf207afacfb3cf40fdc5bddb595cfd9d044
jehveiga <gelsvsampa@hotmail.com>
2023-04-02 18:19:01 -0300

    Criar a controller de usuário

---
+++ SistemasDeTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Usuario>> GetAllUsers()
+        {
+
+
+            return Ok(); // retorna o código 200
+        }
+    }
+}
