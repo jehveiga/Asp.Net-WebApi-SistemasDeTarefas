73a49063f94b8b882fe2defd3cd69d546f828f77
jehveiga <gelsvsampa@hotmail.com>
2023-04-02 18:37:03 -0300

    Criar a classe DbContext

---
+++ SistemasDeTarefas/Data/SistemaTarefasDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Models;
+
+namespace SistemasDeTarefas.Data
+{
+    public class SistemaTarefasDbContext : DbContext
+    {
+        public SistemaTarefasDbContext(DbContextOptions<SistemaTarefasDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
