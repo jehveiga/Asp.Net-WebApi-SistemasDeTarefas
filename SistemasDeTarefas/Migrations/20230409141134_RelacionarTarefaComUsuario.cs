c22af373e507e01ce5f109ba83a24ddb6bd1b032
jehveiga <gelsvsampa@hotmail.com>
2023-04-09 11:17:08 -0300

    Adicina a migração de relacionamento da tabela tarefas com a tabela de usuário

---
+++ SistemasDeTarefas/Migrations/20230409141134_RelacionarTarefaComUsuario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemasDeTarefas.Migrations
+{
+    public partial class RelacionarTarefaComUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioId",
+                table: "Tarefas",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_UsuarioId",
+                table: "Tarefas",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tarefas_Usuarios_UsuarioId",
+                table: "Tarefas",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tarefas_Usuarios_UsuarioId",
+                table: "Tarefas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tarefas_UsuarioId",
+                table: "Tarefas");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Tarefas");
+        }
+    }
+}
